6c17ef92d65a4a7577290af36ffa7fef6861478a
cmvazquez <msc.claudia.martinez@gmail.com>
2022-03-22 12:50:47 -0700

    add methods using different access modifiers

--- src/AccessModifiers.java
+++ src/AccessModifiers.java
@@ -1,10 +1,35 @@
 import com.ejemplos.objects.Developer;
 
 public class AccessModifiers {
+	
+	public void methodPublic() {
+		System.out.println("This method is visible within this class,"
+				+ " in other classes within this package and outside this packages.");
+	}
+	
+	void methodPackage() {
+		System.out.println("This method is visible within this class,"
+				+ " in other classes within this package, but not outside this packages.");
+	}
+	
+	protected void methodProtected() {
+		System.out.println("This method is visible within this class,"
+				+ " in other classes within this package and outside this packages only by subclasses.");
+	}
+	
+	private void methodPrivate() {
+		System.out.println("This method is visible only within this class.");
+	}			
+	
 
 	public static void main(String[] args) {
 	
-		Developer dev = new  Developer();
+		AccessModifiers app = new AccessModifiers();
+		
+		app.methodPackage();
+		app.methodPublic();
+		app.methodProtected();
+		app.methodPrivate();
 
 	}
 
